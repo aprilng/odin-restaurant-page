1d7f3401d57ca5b0550857147589717200b6e551
April Nguyen <24437383+aprilng@users.noreply.github.com>
2025-09-18 15:55:04 -0700

    Fix display

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import loadHome from './home';
 import loadMenu from './menu';
 import loadContact from './contact';
 
+/*
 function displayTab(tabLoader) {
   const content = document.getElementById('content');
   content.innerHTML = '';          // clear current content
@@ -15,3 +16,4 @@ displayTab(loadHome);
 document.getElementById('homeBtn').addEventListener('click', () => displayTab(loadHome));
 document.getElementById('menuBtn').addEventListener('click', () => displayTab(loadMenu));
 document.getElementById('contactBtn').addEventListener('click', () => displayTab(loadContact));
+*/
